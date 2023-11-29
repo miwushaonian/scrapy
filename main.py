958166f017d10589e512f6ac71ef40950adb8e6f
golangboy <golangboy@qq.com>
2023-11-29 11:32:37 +0800

    first

---
+++ main.py
@@ -0,0 +1,52 @@
+import cfscrape
+import time
+import sub
+import m3
+import multiprocessing
+import os
+
+os.makedirs("video", exist_ok=True)
+if __name__ == "__main__":
+    i = 2
+    scraper = cfscrape.create_scraper()  # returns a CloudflareScraper instance
+    # Or: scraper = cfscrape.CloudflareScraper()  # CloudflareScraper inherits from requests.Session
+    retrys = 0
+    while True:
+        try:
+            cont = scraper.get(f"https://hsex.men/list-{i}.htm").content
+            cont = cont.decode("utf-8")
+            test_str = str(cont)
+            import re
+
+            regex = r"video-[0-9]*.htm"
+            matches = re.finditer(regex, test_str, re.MULTILINE)
+            matches_url = []
+            for matchNum, match in enumerate(matches, start=1):
+                matches_url.append(match.group())
+            matches_url = list(set(matches_url))
+            if 0 == len(matches_url):
+                if retrys >= 50:
+                    i = 1
+                    retrys = 0
+                    print("No more page, return to 1")
+                else:
+                    print(f"No more page, retry 5 times {retrys}")
+                    retrys = retrys + 1
+                continue
+            else:
+                retrys = 0
+                i = i + 1
+            print(f"Now scrapy page {i},total {len(matches_url)}")
+            for k in matches_url:
+                try:
+                    m3u8_url = sub.get_m3u8(f"https://hsex.men/{k}")
+                    m3.proc([m3u8_url], [f"{k}"])
+                    import shutil
+                    shutil.move(f"{k}.mp4", f"video/{k}.data")
+                except Exception as e:
+                    print(f"下载失败 {k} {e}")
+                    continue
+            pass
+        except Exception as e:
+            print(f"下载失败 {e}")
+            continue
---
+++ sub.py
@@ -0,0 +1,14 @@
+def get_m3u8(url_path):
+    import cfscrape
+
+    scraper = cfscrape.create_scraper()  # returns a CloudflareScraper instance
+    # Or: scraper = cfscrape.CloudflareScraper()  # CloudflareScraper inherits from requests.Session
+    cont = scraper.get(url_path).content
+    cont = cont.decode("utf-8")
+    test_str = str(cont)
+    import re
+
+    regex = r"https.*.m3u8.*?\""
+    matches = re.finditer(regex, test_str, re.MULTILINE)
+    targetUrl = matches.__next__().group()
+    return targetUrl[:-1]
