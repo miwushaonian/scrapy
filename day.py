d72af4d5e31a3d242a3b6cb675a08b4fb036af92
golangboy <golangboy@qq.com>
2023-12-08 15:30:56 +0800

    更新范围更大

--- day.py
+++ day.py
@@ -2,7 +2,7 @@ import time
 import subprocess
 
 while True:
-    shell_cmd = "python main.py -s=127.0.0.1 -p=6333 -page=1 -ep=5"
+    shell_cmd = "python main.py -s=127.0.0.1 -p=6333 -page=1 -ep=20"
     for i in range(60):
         time.sleep(60)
     print(f"第{i}次更新")
