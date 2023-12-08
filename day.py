d82a0b7e91d3dc9ec00055c76c05b53bd8df9eaf
golangboy <golangboy@qq.com>
2023-12-08 15:30:05 +0800

    一小时更新一次

--- day.py
+++ day.py
@@ -3,7 +3,7 @@ import subprocess
 
 while True:
     shell_cmd = "python main.py -s=127.0.0.1 -p=6333 -page=1 -ep=5"
-    for i in range(60*12):
+    for i in range(60):
         time.sleep(60)
     print(f"第{i}次更新")
     subprocess.Popen(shell_cmd).wait()
